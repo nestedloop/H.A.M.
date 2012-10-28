dd30ae76a0ce80f02d67f22b7c1b65fe8e4b4f95
apanzariu <adrianpanzariu@gmail.com>
2012-10-28 05:00:41 +0200

    ManagedApp class - first submit
    Limited functionalitym basically a mock with some stubs :D

---
+++ HamSpace/HamSpace/ManagedApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HamSpace
+{
+    public class HamUtils
+    {
+        public static const string DECOY_NAME = "!";
+        public static const string LOCKED_NAME = "!!";
+    }
+    public class ManagedApp
+    {
+        FileSystemWatcher fileDecoyWatch;
+        FileStream fs;
+        public ManagedApp(string installPath)
+        {
+            string installPathDecoy = installPath + "\\" + HamUtils.DECOY_NAME;
+            string installPathLocked = installPath + "\\" + HamUtils.LOCKED_NAME;
+            this.fileDecoyWatch = new FileSystemWatcher(installPathDecoy);
+            fs = File.Open(installPathLocked, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            this.fileDecoyWatch.Deleted += new FileSystemEventHandler(fileDecoyWatch_Deleted);
+        }
+
+        void fileDecoyWatch_Deleted(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
